cd4b24802f0e40e13161208fef1428be023e6665
Ardhifm <ardhihidayad@gmail.com>
2021-11-07 09:09:23 +0700

    Sistem Perpustakaan

---
+++ README.md
@@ -0,0 +1,113 @@
+
+# Sistem Informasi Perpustakaan
+Sistem infonformasi perpustakaan yang dibangun mengunakan PHP
+
+
+## Installation
+
+``bash download https://github.com/salmonella-code/sistem-perpustakaan.git
+```
+
+Pergi ke directory project
+
+```bash
+  cd perpustakaan
+```
+
+Import Database dbperpus2
+
+```bash
+  buka phpmyadmin kemudian buat Database dengan nama dbperpus2 setelah itu Import file dbperpus2 yang ada pada directory project ini untuk mendapatkan tabel beserta semua isinya.
+```
+
+Configuration Connection DB
+
+```bash
+  buka file koneksi.php yang terletak pada directory controller kemudian sesuaikan user, password, nama database yang telah kalian buat sebelumnya.
+```
+
+Cara Menggunakan
+
+```bash
+  buka browser kalian apa saja, kemudian pada bagian tab url ketikan (Sebagai Contoh): http://localhost/perpustakaan/   maka secara otomatis aka menampilkan halaman login
+  ```
+  
+## User
+
+user login yang tersimpan pada website ini adalah
+
+- username = `admin`
+- Password = `admin`
+
+  
+## Badges
+
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![Css](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![Js](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![php](https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white)
+![mysql](https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white)
+![github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)
+
+## Tree directory
+```bash
+.
+├── Asset
+│   ├── css
+│   ├── images
+│   ├── js
+│   ├── scss
+│   ├── vendor
+├── pages
+│   ├── anggota.php
+│   ├── anggota-edit.php
+│   ├── beranda.php
+│   ├── buku.php
+│   ├── buku-edit.php
+│   ├── cetak_info_buku.php
+│   ├── cetak_kartu.php
+│   ├── cetak-data-anggota.php
+│   ├── cetak-data-buku.php
+│   ├── cetak-data-transaksi-peminjaman.php
+│   ├── cetak-data-transaksi-pengembalian.php
+│   ├── cetak-info-transaksi-peminjaman.php
+│   ├── cetak-info-transaksi-pengembalian.php
+│   ├── data-buku.php
+│   ├── transaksi-edit-peminjaman.php
+│   ├── transaksi-edit-pengembalian.php
+│   ├── transaksi-edit-peminjaman.php
+│   ├── transaksi-peminjaman.php
+│   └── transaksi-pengembalian.php
+├── proses
+│   ├── anggota-edit-proses.php
+│   ├── anggota-hapus.php
+│   ├── anggota-input-proses.php
+│   ├── buku-edit-proses.php
+│   ├── buku-hapus.php
+│   ├── buku-input-proses.php
+│   ├── buku-edit-proses.php
+│   ├── transaksi-edit-proses.php
+│   ├── transaksi-input-proses.php
+│   ├── transaksi-peminjaman-hapus.php
+│   ├── transaksi-peminjaman-edit.php
+│   └── transaksi-pengembalian-hapus.php
+├── cek_login.php
+├── dbperpus2.sql
+├── index.php
+├── koneksi.php
+├── login.php
+├── logout.php
+└── README.md
+```
+
+  
+## Authors
+
+- [@Muhammad Ardhi Hidayad](https://github.com/Ardhifm)
+
+## 🚀 About Me
+I'm a junior web developer...
+
+  
+# sistem-perpustakaan
+# sistem-perpustakaan
---
+++ pages/beranda.php
@@ -0,0 +1,98 @@
+
+<!-- Begin Page Content --> 
+<div class="container-fluid">  
+<h3><center>SELAMAT DATANG DI SISTEM INFORMASI PERPUSTAKAAN</center></h3>
+<P></P>
+<h4><center> "SILAHKAN GUNAKAN MENU YANG BERADA DI BAWAH DAN DISAMPING" </center></h4>
+<br>
+  <!-- row ux-->   
+  <div class="row">     
+    <div class="col-xl-3 col-md-6 mb-4">       
+      <div class="card border-left-danger shadow h-100 py-2 bg-info"> 
+
+        <div class="card-body">           
+          <div class="row no-gutters align-items-center">             
+            <div class="col mr-2">
+            <a class="nav-link" href="index.php?p=anggota">
+			
+
+            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Jumlah Anggota</div>               
+            <div class="h1 mb-0 font-weight-bold text-white">
+            </div>          
+
+          </div> 
+
+          <div class="col-auto">     
+            <i class="fas fa-users fa-3x text-warning"></i></a>             
+          </div>           
+        </div>         
+      </div>       
+    </div>     
+  </div>  
+
+  <div class="col-xl-3 col-md-6 mb-4"> 
+    <div class="card border-left-primary shadow h-100 py-2 bg-info">         
+
+    <div class="card-body">           
+      <div class="row no-gutters align-items-center">             
+      <div class="col mr-2">      
+      <a class="nav-link" href="index.php?p=buku">         
+
+        <div class="text-md font-weight-bold text-white text-uppercase mb-1">Data Buku</div>               
+        <div class="h1 mb-0 font-weight-bold text-white">                 
+                
+        </div>             
+      </div>             
+      <div class="col-auto">               
+       <i class="fas fa-book fa-3x text-primary"></i></a> 
+        </div>           
+      </div>         
+    </div>       
+  </div>     
+</div>  
+    
+    <div class="col-xl-3 col-md-6 mb-4"> 
+
+      <div class="card border-left-success shadow h-100 py-2 bg-info">         
+        <div class="card-body">           
+          <div class="row no-gutters align-items-center">             
+            <div class="col mr-2">    
+            <a class="nav-link" href="index.php?p=transaksi-peminjaman">           
+              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Transaksi Peminjaman</div>               
+              <div class="h1 mb-0 font-weight-bold text-white">                 
+                        
+            </div>             
+          </div>             
+
+          <div class="col-auto">               
+          <i class="fas fa-user-tag fa-3x text-success"></i></a>             
+          </div>           
+        </div>         
+      </div>       
+    </div>     
+  </div>
+
+  <div class="col-xl-3 col-md-6 mb-4">       
+    <div class="card border-left-warning shadow h-100 py-2 bg-info">         
+      <div class="card-body">           
+        <div class="row no-gutters align-items-center">             
+          <div class="col mr-2">  
+          <a class="nav-link" href="index.php?p=transaksi-pengembalian">             
+            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Transaksi Pengembalian</div>      
+            <div class="h1 mb-0 font-weight-bold text-white">                 
+           
+          </div>             
+        </div>
+
+        <div class="col-auto">               
+         <i class="fas fa-shopping-cart fa-3x text-danger"></i></a>             
+        </div>           
+      </div>         
+    </div>       
+  </div>     
+</div>   
+</div>   
+
+ <!-- /.container-fluid -->  
+ 
+</div> <!-- End of Main Content -->
